09ba49464b360021f01921d748fa721dfdaed1cc
possn <pedrossnunes@gmail.com>
2026-09-09 20:32:34 +0200

    Fix persisted ESEF coverage reporting (#284)
    * Count persisted ESEF provenance in coverage audit
    
    * Cover persisted ESEF provenance in audit

--- tests/test_esef_coverage_audit.py
+++ tests/test_esef_coverage_audit.py
@@ -27,6 +27,35 @@ class EsefCoverageAuditTests(unittest.TestCase):
         self.assertEqual(audit['by_suffix']['.LS']['country'], 'PT')
         self.assertEqual(audit['isin_sources']['Euronext official equities list'], 1)
 
+    def test_persisted_esef_provenance_counts_when_runtime_marker_was_not_serialized(self):
+        audit = build_audit([
+            {
+                'ticker': 'ABDN.L',
+                'quote_type': 'EQUITY',
+                'isin': 'GB00BF8Q6K64',
+                'lei': '213800Q2RPIT9B7R9Z02',
+                'data_sources': ['Yahoo Finance', 'ESEF / filings.xbrl.org'],
+            }
+        ])
+        totals = audit['totals']
+        self.assertEqual(totals['eligible'], 1)
+        self.assertEqual(totals['with_isin'], 1)
+        self.assertEqual(totals['with_lei'], 1)
+        self.assertEqual(totals['filing_found'], 1)
+        self.assertEqual(totals['esef_enriched'], 1)
+        self.assertEqual(totals['enriched_rate'], 1.0)
+
+    def test_unrelated_provenance_does_not_create_false_esef_enrichment(self):
+        audit = build_audit([
+            {
+                'ticker': 'TEST.PA',
+                'quote_type': 'EQUITY',
+                'data_sources': ['Yahoo Finance'],
+            }
+        ])
+        self.assertEqual(audit['totals'].get('esef_enriched', 0), 0)
+        self.assertEqual(audit['totals']['enriched_rate'], 0.0)
+
 
 if __name__ == '__main__':
     unittest.main()
