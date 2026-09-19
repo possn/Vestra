90dfd6237c6cfa7eb62a6dd2d732adb636f212a0
possn <pedrossnunes@gmail.com>
2026-09-19 18:12:47 +0200

    Defer politicians module until Market first use

--- market-static-universe.js
+++ market-static-universe.js
@@ -1,4 +1,4 @@
-/* Vestra Market static universe loader v1.13 */
+/* Vestra Market static universe loader v1.14 */
 (() => {
   'use strict';
 
@@ -108,6 +108,7 @@
   function ensureMobileUiRefresh() { loadCompanion('VestraMobileUiRefresh','script[data-vestra-mobile-ui-refresh]','mobile-ui-refresh.js?v=1.4','vestraMobileUiRefresh'); }
   function ensureMarketUiPolish() { loadCompanion('VestraMarketUiPolish','script[data-vestra-market-ui-polish]','market-ui-polish.js?v=1.3&stockthemes=2','vestraMarketUiPolish'); }
   function ensureUiVisualPolish() { loadCompanion('VestraUiVisualPolish','script[data-vestra-ui-visual-polish]','ui-visual-polish.js?v=1.1','vestraUiVisualPolish'); }
+  function ensurePoliticiansCompanion() { loadCompanion('VestraPoliticians','script[data-vestra-politicians]','politicians.js?v=2.1','vestraPoliticians'); }
 
   function ensureMarketCompanions() {
     // Market-only helpers are deliberately deferred until Market is first used.
@@ -116,6 +117,7 @@
     ensureScannerCompanion();
     ensureAnalysisToolsRuntime();
     ensureMarketUiPolish();
+    ensurePoliticiansCompanion();
     return ensureEtfIntelligence();
   }
 
@@ -213,8 +215,8 @@
   window.VestraMarketStaticUniverse = Object.freeze({
     create, getStocks, ensureEtfIntelligence, ensureScannerCompanion, ensureAnalysisToolsRuntime,
     ensureWeeklyEventsCompanion, ensureWeeklyEventsNavigation, ensureDashboardUiRefresh, ensureDashboardDailyNews,
-    ensureMobileUiRefresh, ensureMarketUiPolish, ensureUiVisualPolish, ensureMarketCompanions, unpackStartupPayload,
+    ensureMobileUiRefresh, ensureMarketUiPolish, ensureUiVisualPolish, ensurePoliticiansCompanion, ensureMarketCompanions, unpackStartupPayload,
     dataFetchTimeoutMs: DATA_FETCH_TIMEOUT_MS, etfIntelligenceLoadTimeoutMs: ETF_INTELLIGENCE_LOAD_TIMEOUT_MS,
-    companionLoadTimeoutMs: COMPANION_LOAD_TIMEOUT_MS, version: '1.13',
+    companionLoadTimeoutMs: COMPANION_LOAD_TIMEOUT_MS, version: '1.14',
   });
 })();
