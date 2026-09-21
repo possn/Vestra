9c8a72de82bffcc8ccbf7786079699271c42e0af
possn <pedrossnunes@gmail.com>
2026-09-21 11:09:19 +0200

    Exercise service workers in WebKit tests

--- tests/e2e/playwright.production.config.js
+++ tests/e2e/playwright.production.config.js
@@ -20,7 +20,7 @@ module.exports = defineConfig({
     trace: 'retain-on-failure',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
-    serviceWorkers: 'block'
+    serviceWorkers: 'allow'
   },
   projects: [
     {
