50344fcbf2855db9ce1ed7b8100bfe4d8d2786d4
possn <pedrossnunes@gmail.com>
2026-09-25 22:45:41 +0200

    Align early Market loader version contract

--- tests/test_market_runtime_early_mode.py
+++ tests/test_market_runtime_early_mode.py
@@ -22,8 +22,8 @@ class MarketRuntimeEarlyModeTests(unittest.TestCase):
         self.assertIn("button.dispatchEvent(new MouseEvent('click'", self.loader)
 
     def test_bootstrap_uses_current_runtime_loader_version(self):
-        self.assertIn('market-runtime-loader.js?v=1.5', self.index)
-        self.assertIn("version: '1.5'", self.loader)
+        self.assertIn('market-runtime-loader.js?v=1.6', self.index)
+        self.assertIn("version: '1.6'", self.loader)
 
 
 if __name__ == '__main__':
